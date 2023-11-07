7349ac455a3587ce2ba48550f4c4a6b5a2392109
Brian Lemons <zitronen25u@gmail.com>
2023-11-07 14:01:33 -0800

    Create log_creation.ps1

---
+++ log_creation.ps1
@@ -0,0 +1,21 @@
+# UPDATE LOG FILE
+$DateFormat = "yyyy-MM-dd"
+$Today = Get-Date -Format $DateFormat
+$LogFolder = "C:\Code\<projectName>\Logs\"
+$TodayLog = $LogFolder+$Today+"_Logs.log"
+
+# For appending log file entries
+function Write-TodayLogFile ($log) {
+    filter timestamp {"$(Get-Date -Format G) - $PSItem"}
+    $log | timestamp | out-file $TodayLog -append
+}
+
+# call this function to add log entries. 
+
+<# Example
+
+Write-TodayLogFile($log + "pass") 
+
+will print a log file with the above DTstamp and then write pass after that
+
+#>
